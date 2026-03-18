c9c04cc3bbdc3a38437bedb15963250af517401e
MakaylaMcCreary <makayla.mccreary@outlook.com>
2026-03-18 12:52:30 -0400

    main function

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from bestSeeds import BestSeeds
 from extendSeeds import extendFromSeeds
+from TwoHit import TwoHitSeeds
 
 # Input:
 # Output:
-def miniBLAST(ref, query, k, matchReward, mismatchPen, gapOpenPen, gapExtendPen, threshHSSP, Xdrop):
-    seeds = BestSeeds(ref, query, k, matchReward, mismatchPen, threshHSSP)
-    bestLocalAlignment = extendFromSeeds(ref, query, seeds, k, matchReward, mismatchPen, gapOpenPen, gapExtendPen, Xdrop)
-    pass
+def miniBLAST(ref, query, k, matchReward, mismatchPen, gapOpenPen, gapExtendPen, threshHSSP, Xdrop, A):
+    singleSeeds = BestSeeds(ref, query, k, matchReward, mismatchPen, threshHSSP)
+    
+    seedsToExtend = TwoHitSeeds(singleSeeds, k, A)
+    
+    bestLocalAlignment = extendFromSeeds(ref, query, seedsToExtend, k, matchReward, mismatchPen, gapOpenPen, gapExtendPen, Xdrop)
+    
+    return bestLocalAlignment
