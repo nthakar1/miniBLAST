2bcd098ddc567918388b1d26bfc6dad62769e73b
vedikasridhar <sridharvedika@gmail.com>
2026-03-26 15:17:52 -0400

    created BlastConfig class with blastn and blastp objects. Updated miniblastn function to include blastn parameters. blastp parameters will be added to a separate miniblastp function later

--- datatypes.py
+++ datatypes.py
@@ -1,3 +1,70 @@
+from dataclasses import dataclass
+from typing import Optional
+BLOSUM = dict()
+BLOSUM['A'] = {'A': 4, 'C': 0, 'D': -2, 'E': -1, 'F': -2, 'G': 0, 'H': -2, 'I': -1, 'K': -1, 'L': -1, 'M': -1, 'N': -2, 'P': -1, 'Q': -1, 'R': -1, 'S': 1, 'T': 0, 'V': 0, 'W': -3, 'Y': -2}
+BLOSUM['C'] = {'A': 0, 'C': 9, 'D': -3, 'E': -4, 'F': -2, 'G': -3, 'H': -3, 'I': -1, 'K': -3, 'L': -1, 'M': -1, 'N': -3, 'P': -3, 'Q': -3, 'R': -3, 'S': -1, 'T': -1, 'V': -1, 'W': -2, 'Y': -2}
+BLOSUM['D'] = {'A': -2, 'C': -3, 'D': 6, 'E': 2, 'F': -3, 'G': -1, 'H': -1, 'I': -3, 'K': -1, 'L': -4, 'M': -3, 'N': 1, 'P': -1, 'Q': 0, 'R': -2, 'S': 0, 'T': -1, 'V': -3, 'W': -4, 'Y': -3}
+BLOSUM['E'] = {'A': -1, 'C': -4, 'D': 2, 'E': 5, 'F': -3, 'G': -2, 'H': 0, 'I': -3, 'K': 1, 'L': -3, 'M': -2, 'N': 0, 'P': -1, 'Q': 2, 'R': 0, 'S': 0, 'T': -1, 'V': -2, 'W': -3, 'Y': -2}
+BLOSUM['F'] = {'A': -2, 'C': -2, 'D': -3, 'E': -3, 'F': 6, 'G': -3, 'H': -1, 'I': 0, 'K': -3, 'L': 0, 'M': 0, 'N': -3, 'P': -4, 'Q': -3, 'R': -3, 'S': -2, 'T': -2, 'V': -1, 'W': 1, 'Y': 3}
+BLOSUM['G'] = {'A': 0, 'C': -3, 'D': -1, 'E': -2, 'F': -3, 'G': 6, 'H': -2, 'I': -4, 'K': -2, 'L': -4, 'M': -3, 'N': 0, 'P': -2, 'Q': -2, 'R': -2, 'S': 0, 'T': -2, 'V': -3, 'W': -2, 'Y': -3}
+BLOSUM['H'] = {'A': -2, 'C': -3, 'D': -1, 'E': 0, 'F': -1, 'G': -2, 'H': 8, 'I': -3, 'K': -1, 'L': -3, 'M': -2, 'N': 1, 'P': -2, 'Q': 0, 'R': 0, 'S': -1, 'T': -2, 'V': -3, 'W': -2, 'Y': 2}
+BLOSUM['I'] = {'A': -1, 'C': -1, 'D': -3, 'E': -3, 'F': 0, 'G': -4, 'H': -3, 'I': 4, 'K': -3, 'L': 2, 'M': 1, 'N': -3, 'P': -3, 'Q': -3, 'R': -3, 'S': -2, 'T': -1, 'V': 3, 'W': -3, 'Y': -1}
+BLOSUM['K'] = {'A': -1, 'C': -3, 'D': -1, 'E': 1, 'F': -3, 'G': -2, 'H': -1, 'I': -3, 'K': 5, 'L': -2, 'M': -1, 'N': 0, 'P': -1, 'Q': 1, 'R': 2, 'S': 0, 'T': -1, 'V': -2, 'W': -3, 'Y': -2}
+BLOSUM['L'] = {'A': -1, 'C': -1, 'D': -4, 'E': -3, 'F': 0, 'G': -4, 'H': -3, 'I': 2, 'K': -2, 'L': 4, 'M': 2, 'N': -3, 'P': -3, 'Q': -2, 'R': -2, 'S': -2, 'T': -1, 'V': 1, 'W': -2, 'Y': -1}
+BLOSUM['M'] = {'A': -1, 'C': -1, 'D': -3, 'E': -2, 'F': 0, 'G': -3, 'H': -2, 'I': 1, 'K': -1, 'L': 2, 'M': 5, 'N': -2, 'P': -2, 'Q': 0, 'R': -1, 'S': -1, 'T': -1, 'V': 1, 'W': -1, 'Y': -1}
+BLOSUM['N'] = {'A': -2, 'C': -3, 'D': 1, 'E': 0, 'F': -3, 'G': 0, 'H': 1, 'I': -3, 'K': 0, 'L': -3, 'M': -2, 'N': 6, 'P': -2, 'Q': 0, 'R': 0, 'S': 1, 'T': 0, 'V': -3, 'W': -4, 'Y': -2}
+BLOSUM['P'] = {'A': -1, 'C': -3, 'D': -1, 'E': -1, 'F': -4, 'G': -2, 'H': -2, 'I': -3, 'K': -1, 'L': -3, 'M': -2, 'N': -2, 'P': 7, 'Q': -1, 'R': -2, 'S': -1, 'T': -1, 'V': -2, 'W': -4, 'Y': -3}
+BLOSUM['Q'] = {'A': -1, 'C': -3, 'D': 0, 'E': 2, 'F': -3, 'G': -2, 'H': 0, 'I': -3, 'K': 1, 'L': -2, 'M': 0, 'N': 0, 'P': -1, 'Q': 5, 'R': 1, 'S': 0, 'T': -1, 'V': -2, 'W': -2, 'Y': -1}
+BLOSUM['R'] = {'A': -1, 'C': -3, 'D': -2, 'E': 0, 'F': -3, 'G': -2, 'H': 0, 'I': -3, 'K': 2, 'L': -2, 'M': -1, 'N': 0, 'P': -2, 'Q': 1, 'R': 5, 'S': -1, 'T': -1, 'V': -3, 'W': -3, 'Y': -2}
+BLOSUM['S'] = {'A': 1, 'C': -1, 'D': 0, 'E': 0, 'F': -2, 'G': 0, 'H': -1, 'I': -2, 'K': 0, 'L': -2, 'M': -1, 'N': 1, 'P': -1, 'Q': 0, 'R': -1, 'S': 4, 'T': 1, 'V': -2, 'W': -3, 'Y': -2}
+BLOSUM['T'] = {'A': 0, 'C': -1, 'D': -1, 'E': -1, 'F': -2, 'G': -2, 'H': -2, 'I': -1, 'K': -1, 'L': -1, 'M': -1, 'N': 0, 'P': -1, 'Q': -1, 'R': -1, 'S': 1, 'T': 5, 'V': 0, 'W': -2, 'Y': -2}
+BLOSUM['V'] = {'A': 0, 'C': -1, 'D': -3, 'E': -2, 'F': -1, 'G': -3, 'H': -3, 'I': 3, 'K': -2, 'L': 1, 'M': 1, 'N': -3, 'P': -2, 'Q': -2, 'R': -3, 'S': -2, 'T': 0, 'V': 4, 'W': -3, 'Y': -1}
+BLOSUM['W'] = {'A': -3, 'C': -2, 'D': -4, 'E': -3, 'F': 1, 'G': -2, 'H': -2, 'I': -3, 'K': -3, 'L': -2, 'M': -1, 'N': -4, 'P': -4, 'Q': -2, 'R': -3, 'S': -3, 'T': -2, 'V': -3, 'W': 11, 'Y': 2}
+BLOSUM['Y'] = {'A': -2, 'C': -2, 'D': -3, 'E': -2, 'F': 3, 'G': -3, 'H': 2, 'I': -1, 'K': -2, 'L': -1, 'M': -1, 'N': -2, 'P': -3, 'Q': -1, 'R': -2, 'S': -2, 'T': -2, 'V': -1, 'W': 2, 'Y': 7}
+
+
+# using dataclass for easier formatting of classes and objects
+@dataclass
+# all parameter information found via https://www.ncbi.nlm.nih.gov/books/NBK279684/ see table C2 and C3
+class BlastConfig:
+    # parameters that will be included in both blastn and blastp objects
+    name: str 
+    match_reward: int
+    mismatch_penalty: int
+    gap_opening: int
+    gap_extension: int
+    k_mer_size: int
+    # used only in protein seq, so default is none for nucleotides and we can update matrix=BLOSUM for next blastp object
+    matrix: Optional[dict] = None 
+    # set x drop values equal to blastn defaults, all are in bits (ie statistically normalized)
+    xdrop_ungap: int = 20
+    xdrop_gap: int = 30
+    xdrop_gap_final: int = 100
+    threshold_T: int = 0
+
+# blastn parameter is used to compare dna sequences
+BLASTN_PARAMS = BlastConfig(
+    name="blastn",
+    match_reward=2,
+    mismatch_penalty=-3,
+    gap_opening=5,
+    gap_extension=2,
+    k_mer_size=7
+    # x drop defaults auto applied
+)
+
+BLASTP_PARAMS = BlastConfig(
+    name="blastp",
+    match_reward=0, 
+    mismatch_penalty=0, 
+    gap_opening=11,
+    gap_extension=1,
+    k_mer_size=3,
+    matrix=BLOSUM, 
+    xdrop_gap_final=25, # heuristic value in bits for final gapped alignment, as long as Maximum Score Observed - Current Score <x we can continue, otherwise we trim alignment back to where the max score was found
+    threshold_T=11 # need a score of >=11 to seed a kmer that will move to the extension phase (relatively low to increase sensitivity)
+)
+
 
 # Not actually using these currently but we'll see
 
@@ -27,3 +94,4 @@ class BST:
         node.right = self._build(d, midIndex+1, rightIndex)
 
         return node
+    
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from bestSeeds import BestSeeds
 from extendSeeds import extendFromSeeds
 from TwoHit import TwoHitSeeds
+from datatypes import BLASTN_PARAMS, BLASTP_PARAMS, BlastConfig
 
 # Input:
 # Output:
-def miniBLAST(ref, query, k, matchReward, mismatchPen, gapOpenPen, gapExtendPen, threshHSSP, Xdrop, A):
+def miniBLASTn(ref, query, threshHSSP, A):
+    k=BLASTN_PARAMS.k_mer_size
+    matchReward=BLASTN_PARAMS.match_reward
+    mismatchPen=BLASTN_PARAMS.mismatch_penalty
+    gapOpenPen=BLASTN_PARAMS.gap_opening
+    gapExtendPen=BLASTN_PARAMS.gap_extension
+    Xdrop=BLASTN_PARAMS.xdrop_gap_final
+
     singleSeeds = BestSeeds(ref, query, k, matchReward, mismatchPen, threshHSSP)
     print("Seeds1:", singleSeeds)
     
