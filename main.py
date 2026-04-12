ed22b0c6843febdeb657d49efd1c4e92d70bd71d
MakaylaMcCreary <makayla.mccreary@outlook.com>
2026-04-12 13:47:10 -0400

    small changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bestSeeds import BestSeeds
 from bestSeeds import GenerateAllKmers
 from bestSeeds import EncodedIndexation
 from extendSeeds import extendFromSeeds
-from TwoHit import TwoHitSeeds
+from twoHit import TwoHitSeeds
 from datatypes import BLASTN_PARAMS, BLASTP_PARAMS, BlastConfig, BLOSUM
 from database import fetch_mixed_database
 from statistics import calculate_bit_score, calculate_e_value
@@ -140,7 +140,6 @@ def main():
     
     # could consider computing once and storing--time vs memory
     db_h1n1 = fetch_mixed_database(queriesViral, "h1n1_mixed_database.fasta")
-    # BUILD AND SAVE ENCODED INDEXATION FILE FOR WHOLE DATABASE RATHER THAN COMPUTING FOT EACH REFERENCE
 
     sum(i**2 for i in range(1000000))
     endTime = time.perf_counter()
